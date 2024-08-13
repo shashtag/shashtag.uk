c768c8112fa77bc9dbbf3be0603d3a2a6b67219a
Shashwat Gupta <shashwatsatna@gmail.com>
2024-08-13 10:25:15 +0530

    preload images

--- src/HOC/Card/Card.jsx
+++ src/HOC/Card/Card.jsx
@@ -29,13 +29,13 @@ const Card = ({ children }) => {
       className={`bg-gradient-to-b from-[#F5F2EE] to-[#DCD9D5] ml-8 lg:ml-0 w-[calc(100vw-32px)]  lg:w-[1000px] h-[100vh] lg:h-[571.428571429px] lg:rounded-2xl z-20 relative [--move-x:-100vw] lg:[--move-x:0] [--move-y:0] lg:[--move-y:-80vh] flex flex-col `}
       style={{ filter: "drop-shadow(0px 10px 20px)" }}>
       <img
-        loading='lazy'
+        rel='preload'
         src='/assets/hands/hands-sm.svg'
         alt='hands small screen'
         className='absolute lg:hidden  -translate-x-[68.3%] h-[100vh]  top-[50%] -translate-y-1/2 '
       />
       <img
-        loading='lazy'
+        rel='preload'
         src='/assets/hands/hands-lg.png'
         alt='hands large screen'
         className='absolute lg:bottom-[392.5px] hidden lg:block'
