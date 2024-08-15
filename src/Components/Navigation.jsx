3ec7e5627119eab3bbc92ac702c8ff2d42ee98e2
Shashwat Gupta <shashwatsatna@gmail.com>
2024-08-15 08:36:54 +0530

    links

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import nav from "../Data/Navigation";
 
 const Navigation = () => {
+  const location = useLocation();
+
   return (
     <div className='h-full hidden lg:block ml-4'>
       <div className='text-3xl pb-5'>ABOUT</div>
@@ -9,7 +11,10 @@ const Navigation = () => {
         <Link
           key={item.path}
           to={item.path}
-          className='pb-2 block text-lg cursor-pointer hover:tracking-wider transition-all '>
+          className='pb-2 block text-lg cursor-pointer hover:tracking-wider transition-all '
+          style={{
+            letterSpacing: item.path === location.pathname && "0.05em",
+          }}>
           {item.text}
         </Link>
       ))}
